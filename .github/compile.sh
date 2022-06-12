7135b8047017ca501f4db40cd25bd53df9b47882
saladthieves <saladthieves@gmail.com>
2022-06-12 19:09:26 +0300

    ci: Add shed cache on pre-review.yaml file #7

--- .github/compile.sh
+++ .github/compile.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# Compiles the `shed` tool located in the `./build/shed` directory (downloaded by a prior step), and adds the resulting
-# binary `shed` (located in the same directory) to the `$GITHUB_PATH` environment variable. This makes it available to
-# subsequent steps.
-cd ./build/shed
+cd ./build/shed/src || exit
 dart pub get
-dart compile exe bin/shed.dart -o shed
-cd ..
-echo "./build/shed" >> $GITHUB_PATH
+dart compile exe bin/shed.dart -o tool
+cd ../
+mkdir out
+mv ./src/tool ./out/shed
